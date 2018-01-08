c56e5aab9c0155b112e1eb57e49591fdffa24a16
robertocapah <roberto_capah@yahoo.com>
2018-01-08 14:50:00 +0700

    Initial commit

---
+++ app/src/main/java/net/roberto/mylibrarytesting/MainActivity.java
@@ -0,0 +1,13 @@
+package net.roberto.mylibrarytesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
